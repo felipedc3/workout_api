6b3408ca829252725a4ce5c4b55f405d0df1fa1b
Felipe Vieira da Costa <156626738+felipedc3@users.noreply.github.com>
2025-08-12 11:24:21 -0300

    response de retorno de endpoints customizados

--- workout_api/atleta/schemas.py
+++ workout_api/atleta/schemas.py
@@ -27,3 +27,11 @@ class AtletaOut(Atleta, OutMixin):
 class AtletaUpdate(BaseSchema):
     nome: Annotated[Optional[str], Field(None, description='Nome do atleta', example='Joao', max_length=50)]
     idade: Annotated[Optional[int], Field(None, description='Idade do atleta', example=25)]
+
+class AtletaOutCustom(BaseSchema):
+    nome: Annotated[str, Field(description='Nome do atleta', example='João', max_length=50)]
+    centro_treinamento:Annotated[str, Field(description='Nome do centro de treinamento', example='CT King', max_length=20)]
+    categoria: Annotated[str, Field(description='Nome da categoria', example='Scale', max_length=10)]
+
+    class Config:
+        orm_mode = True
